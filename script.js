1464381f793cc8bec640c911f0bb5a9781d11d4e
otomdee <otomdaniel65@gmail.com>
2024-07-04 17:17:03 +0100

    add list item delete button

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const ul = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
+//num gives each new list item a unique id
+let num = 0;
 button.addEventListener("click", () => {
     formText = input.value;
     input.value = "";
     listItem = document.createElement("li");
+    listItem.id = `list${num}`
+    //listNum stores the id of the list item permanently
+    const listNum = num;
+    num++;
     listSpan = document.createElement("span");
     delButton = document.createElement("button");
+
+    ul.appendChild(listItem);
+    listItem.appendChild(listSpan);
+    listItem.appendChild(delButton);
+
+    listSpan.innerText = formText;
+    delButton.innerText = "Delete";
+
+    delButton.addEventListener("click", () => {
+        ul.removeChild(document.querySelector(`#list${listNum}`));
+        num--;
+    })
 })
